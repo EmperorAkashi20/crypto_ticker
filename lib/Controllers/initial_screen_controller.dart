dcf08b379047b86f2506c6294e0f6d7031146ed4
Rishabh Sethia <rishabh@Skynet.local>
2022-08-19 16:35:54 +0530

    Added stream builder to update the data whenever there is a change in the value of a particular asset

--- lib/Controllers/initial_screen_controller.dart
+++ lib/Controllers/initial_screen_controller.dart
@@ -1,33 +1,49 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:crypto_ticker/Models/ResponseModels/all_assets_response_model.dart';
 import 'package:crypto_ticker/Services/url.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 import '../Services/core_services.dart';
 
 class InitialScreenController extends GetxController {
   Rx<AllAssetsResponseModel> allAssetsResponseModel =
       AllAssetsResponseModel().obs;
+  final StreamController<AllAssetsResponseModel> streamController =
+      StreamController();
   @override
   onInit() {
-    Timer(const Duration(seconds: 0), () {
-      Get.isDialogOpen ?? true
-          ? const Offstage()
-          : Get.dialog(const Center(child: CircularProgressIndicator()),
-              barrierDismissible: false);
-      getAssets();
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      getCryptoPrice();
     });
     super.onInit();
   }
 
 //!add a query param as "?limit=10" to get 10 assets
 
-  getAssets() async {
-    debugPrint(baseUrl + getCoinList);
-    var data = await CoreService().getWithoutAuth(url: baseUrl + getCoinList);
+  // getAssets() async {
+  //   debugPrint(baseUrl + getCoinList);
+  //   var data = await CoreService().getWithoutAuth(url: baseUrl + getCoinList);
+  //   if (data == null) {
+  //     if (Get.isDialogOpen ?? false) Get.back();
+  //     return;
+  //   } else {
+  //     if (data['data'] != null) {
+  //       if (Get.isDialogOpen ?? false) Get.back();
+  //       allAssetsResponseModel.value = AllAssetsResponseModel.fromJson(data);
+  //     }
+  //   }
+  // }
+
+  Future<void> getCryptoPrice() async {
+    var url = Uri.parse(baseUrl + getCoinList);
+    debugPrint(url.toString());
+    final response = await http.get(url);
+    final data = json.decode(response.body);
     if (data == null) {
       if (Get.isDialogOpen ?? false) Get.back();
       return;
@@ -35,6 +51,7 @@ class InitialScreenController extends GetxController {
       if (data['data'] != null) {
         if (Get.isDialogOpen ?? false) Get.back();
         allAssetsResponseModel.value = AllAssetsResponseModel.fromJson(data);
+        streamController.sink.add(allAssetsResponseModel.value);
       }
     }
   }
--- lib/Views/initial_screen.dart
+++ lib/Views/initial_screen.dart
@@ -1,6 +1,6 @@
 import 'package:crypto_ticker/Controllers/initial_screen_controller.dart';
-import 'package:crypto_ticker/DeviceManager/screen_constants.dart';
 import 'package:crypto_ticker/DeviceManager/text_styles.dart';
+import 'package:crypto_ticker/Models/ResponseModels/all_assets_response_model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -16,53 +16,79 @@ class InitialScreen extends StatelessWidget {
     double windowHeight = MediaQuery.of(context).size.height;
     double windowWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Obx(
-        () => ListView.builder(
-          itemCount: initialScreenController
-                  .allAssetsResponseModel.value.data?.length ??
-              0,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 2),
-              child: AssetCard(
-                windowHeight: windowHeight,
-                windowWidth: windowWidth,
-                id: initialScreenController
-                    .allAssetsResponseModel.value.data![index].id
-                    .toString(),
-                symbol: initialScreenController
-                    .allAssetsResponseModel.value.data![index].symbol
-                    .toString(),
-                rank: initialScreenController
-                    .allAssetsResponseModel.value.data![index].rank
-                    .toString(),
-                change24h: double.tryParse(initialScreenController
-                            .allAssetsResponseModel
-                            .value
-                            .data![index]
-                            .changePercent24Hr!)!
-                        .toStringAsFixed(4) +
-                    "%",
-                icon: initialScreenController.allAssetsResponseModel.value
-                        .data![index].changePercent24Hr!
-                        .toString()
-                        .contains('-')
-                    ? Icons.arrow_drop_down
-                    : Icons.arrow_drop_up,
-                iconColor: initialScreenController.allAssetsResponseModel.value
-                        .data![index].changePercent24Hr!
-                        .toString()
-                        .contains('-')
-                    ? Colors.red
-                    : Colors.green,
-                price: double.tryParse(initialScreenController
-                        .allAssetsResponseModel.value.data![index].priceUsd!)!
-                    .toStringAsFixed(2),
-                marketCap: initialScreenController
-                    .allAssetsResponseModel.value.data![index].marketCapUsd
-                    .toString(),
-              ),
-            );
+      body: Center(
+        child: StreamBuilder<AllAssetsResponseModel>(
+          stream: initialScreenController.streamController.stream,
+          builder: (context, snapdata) {
+            switch (snapdata.connectionState) {
+              case ConnectionState.waiting:
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              default:
+                if (snapdata.hasError) {
+                  return const Text('Please Wait....');
+                } else {
+                  return ListView.builder(
+                    itemCount: initialScreenController
+                            .allAssetsResponseModel.value.data?.length ??
+                        0,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 5.0, vertical: 2),
+                        child: AssetCard(
+                          windowHeight: windowHeight,
+                          windowWidth: windowWidth,
+                          id: initialScreenController
+                              .allAssetsResponseModel.value.data![index].id
+                              .toString(),
+                          symbol: initialScreenController
+                              .allAssetsResponseModel.value.data![index].symbol
+                              .toString(),
+                          rank: initialScreenController
+                              .allAssetsResponseModel.value.data![index].rank
+                              .toString(),
+                          change24h: double.tryParse(initialScreenController
+                                      .allAssetsResponseModel
+                                      .value
+                                      .data![index]
+                                      .changePercent24Hr!)!
+                                  .toStringAsFixed(4) +
+                              "%",
+                          icon: initialScreenController.allAssetsResponseModel
+                                  .value.data![index].changePercent24Hr!
+                                  .toString()
+                                  .contains('-')
+                              ? Icons.arrow_drop_down
+                              : Icons.arrow_drop_up,
+                          iconColor: initialScreenController
+                                  .allAssetsResponseModel
+                                  .value
+                                  .data![index]
+                                  .changePercent24Hr!
+                                  .toString()
+                                  .contains('-')
+                              ? Colors.red
+                              : Colors.green,
+                          price: double.tryParse(initialScreenController
+                                  .allAssetsResponseModel
+                                  .value
+                                  .data![index]
+                                  .priceUsd!)!
+                              .toStringAsFixed(2),
+                          marketCap: initialScreenController
+                              .allAssetsResponseModel
+                              .value
+                              .data![index]
+                              .marketCapUsd
+                              .toString(),
+                        ),
+                      );
+                    },
+                  );
+                }
+            }
           },
         ),
       ),
